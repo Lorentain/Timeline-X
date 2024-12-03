c927c3e98ff7160da296ca2844c3a2d8d9e70656
Lorenzo Herrero <lorentain2@gmail.com>
2024-12-03 18:11:19 +0100

    Botón de descripción totalmente terminado. Comienzo con el movimiento de las cartas de la mano.

--- Timeline X/Assets/Scripts/InventoryController/CardInventory.cs
+++ Timeline X/Assets/Scripts/InventoryController/CardInventory.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class CardInventory : MonoBehaviour
@@ -7,20 +8,36 @@ public class CardInventory : MonoBehaviour
 
     [SerializeField] private GameObject handPlayer;
 
-    [SerializeField] private List<CardInfo> inventoryCard;
+    [SerializeField] private List<GameObject> inventoryCard;
 
     [SerializeField] private GameObject prefabCard;
+
+    [SerializeField] private float movementTime;
+
+    [SerializeField] private Ease movementeEase;
 
     public void AñadirCartasComienzo()
     {
         for (int i = 0; i < 3; i++)
         {
-            inventoryCard.Add(deckController.RepartirCarta());
-            Debug.Log(inventoryCard[i]);
-            CardController card = Instantiate(prefabCard,gameObject.transform).GetComponent<CardController>();
+            CardInfo aux = deckController.RepartirCarta();
+            Debug.Log(aux);
+            CardController card = Instantiate(prefabCard, gameObject.transform).GetComponent<CardController>();
+            card.AgregarCardInvetory(this);
             card.AgregarHandPlayer(handPlayer.transform);
-            card.AgregarCardInfo(inventoryCard[i]);
-            prefabCard.transform.position = new Vector3(i-1, 0, 0);
+            card.AgregarCardInfo(aux);
+            inventoryCard.Add(card.gameObject);
+            prefabCard.transform.position = new Vector3(i - 1, 0, 0);
+        }
+    }
+
+    public void MoverCartasInventario(GameObject card)
+    {
+        for(int i = 0; i < inventoryCard.Count; i++) {
+            if(i == inventoryCard.IndexOf(card) + 1) {
+                Debug.Log("Las cartas del inventario se mueven");
+                inventoryCard[i].transform.DOMoveX(inventoryCard[i].transform.position.x - 1, movementTime).SetEase(movementeEase);
+            }
         }
     }
 
